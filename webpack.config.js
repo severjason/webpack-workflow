00368ade067ad51eef2f77dbc689be70194eef0a
severjason <ssever2010@gmail.com>
2017-07-03 00:26:43 +0300

    Added publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, "public/js"),
         filename: "[name].js",
+        publicPath: 'js/'
     },
     resolve: {
         extensions: [".ts", ".js"],
